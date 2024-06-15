79b624d7dbb6ed25a9f96da7178e5ce8b8c4324f
Aloisio-Doerl <aloisio.souza.goncalves.code@gmail.com>
2024-06-15 16:15:56 -0300

    Primeiro programa em Java adicionado

---
+++ First_Java/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("\n Esta é a primeira linha de codigo que escrevo em java, 15/06/2024.\n");
+
+    }
+}
